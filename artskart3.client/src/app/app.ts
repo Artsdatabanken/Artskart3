33e7fd5620aee20fe664d4e156e9fc8389aa8edd
Kristoffer Martinsen <kristoffermartinsen.95@gmail.com>
2026-02-26 12:49:49 +0100

    update nbic package, removed weather forecast

--- artskart3.client/src/app/app.ts
+++ artskart3.client/src/app/app.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit, signal } from '@angular/core';
-import { WeatherForecast } from './weather-forecast';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,25 +7,11 @@ import { WeatherForecast } from './weather-forecast';
   standalone: false,
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  public forecasts: WeatherForecast[] = [];
+export class App {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-    this.getForecasts();
-  }
-
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
+  
 
   protected readonly title = signal('artskart3.client');
 }
