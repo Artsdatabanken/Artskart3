dd84727573aa40383a4191eed5d2232e48f0d059
Kristoffer Martinsen <kristoffermartinsen.95@gmail.com>
2026-03-20 12:03:29 +0100

    fix review

--- Artskart3.WebApp/src/environments/environment.development.ts
+++ Artskart3.WebApp/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
+    production: false,
         applicationInsights: {
             connectionString: "InstrumentationKey=da9ac3c7-fc5b-494b-867f-548eb7d24854;IngestionEndpoint=https://norwayeast-0.in.applicationinsights.azure.com/;LiveEndpoint=https://norwayeast.livediagnostics.monitor.azure.com/;ApplicationId=9f8f9def-d7b1-448a-8976-ef396b76c7e7"
         }
