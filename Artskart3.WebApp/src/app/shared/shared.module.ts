1125f665645a22ac88a41c6be57264b93e86b7e4
DipShre <dipak.shrestha@artsdatabanken.no>
2026-03-23 15:08:14 +0100

    refactor #61

--- Artskart3.WebApp/src/app/shared/shared.module.ts
+++ Artskart3.WebApp/src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 import { HeaderComponent } from './components/header/header.component';
 import { ResizablePanelComponent } from './components/resizable-panel/resizable-panel.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    TranslateModule,
     HeaderComponent,
     ResizablePanelComponent,
   ],
