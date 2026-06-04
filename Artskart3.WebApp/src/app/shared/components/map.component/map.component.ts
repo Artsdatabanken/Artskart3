508938ceff5553636bd7967796b211e30addb881
DipShre <dipak.shrestha@artsdatabanken.no>
2026-06-04 11:06:09 +0200

    Update layer visibility close #66

--- Artskart3.WebApp/src/app/shared/components/map.component/map.component.ts
+++ Artskart3.WebApp/src/app/shared/components/map.component/map.component.ts
@@ -121,6 +121,9 @@ export class MapComponent implements AfterViewInit, OnDestroy {
     baseLayerIds.forEach((id: string) => {
       this.map.setLayerVisibility(id, config.visibleLayers.includes(id));
     });
+
+    this.map.setCenter(config.center);
+    this.map.setZoom(config.zoom);
   }
 
   ngOnDestroy(): void {
