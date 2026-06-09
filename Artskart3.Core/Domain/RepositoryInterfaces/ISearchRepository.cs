aa28a5e7dc71fe9fb09648d8fedac07218552a44
Kenneth Aamodt <kenneth.aamodt@gmail.com>
2026-06-09 08:38:23 +0200

    la til dto for searchtaxons

--- Artskart3.Core/Domain/RepositoryInterfaces/ISearchRepository.cs
+++ Artskart3.Core/Domain/RepositoryInterfaces/ISearchRepository.cs
@@ -1,13 +1,12 @@
 using Artskart3.Core.Application.DTOs;
 using Artskart3.Core.Domain.BusinessModels;
-using Artskart3.Core.Domain.Entities;
 using System.Collections.Generic;
 
 namespace Artskart3.Core.Domain.RepositoryInterfaces
 {
     public interface ISearchRepository
     {
-        Task<IEnumerable<Taxon>> GetTaxonsAsync(string name, int maxCount = 20);
+        Task<IEnumerable<TaxonDto>> GetTaxonsAsync(string name, int maxCount = 20);
         IAsyncEnumerable<LocationModel> GetLocationsAsync(LocationSearchFilterDto? filter = null);
         Task<IEnumerable<AreaMarkerDto>> GetAreasByTypeIdsAsync(params int[] areaTypeIds);
     }
