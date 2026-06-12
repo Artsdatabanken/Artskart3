4801a9ac333be37ea9feeab451ac9bd06c628e08
bo <bo.olesen@artsdatabanken.no>
2026-06-12 12:51:53 +0200

    #250 Enum alert types

---
+++ Artskart3.Core/Domain/Enums/AlertType.cs
@@ -0,0 +1,11 @@
+namespace Artskart3.Core.Domain.Enums
+{
+    public enum AlertType
+    {
+        Danger = 0,
+        Warning = 1,
+        Info = 2,
+        Success = 3,
+        Neutral = 4
+    }
+}
