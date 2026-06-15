627bc42b2f4f7758cdb1ef58782d7aaed07bf53f
bo <bo.olesen@artsdatabanken.no>
2026-06-15 15:06:53 +0200

    #245 Creating the service interface

---
+++ Artskart3.Core/Application/Services/Interfaces/IWarningsService.cs
@@ -0,0 +1,9 @@
+using Artskart3.Core.Domain.BusinessModels;
+
+namespace Artskart3.Core.Application.Services.Interfaces
+{
+    public interface IWarningsService
+    {
+        Task<IEnumerable<WarningModel>> GetAllWarningsAsync();
+    }
+}
