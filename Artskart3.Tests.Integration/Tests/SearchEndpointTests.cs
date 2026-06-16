857d41ba11eeebeb185a11279ed61494b31283d1
DipShre <dipak.shrestha@artsdatabanken.no>
2026-06-16 09:08:34 +0200

    økter max result i test close  #58

--- Artskart3.Tests.Integration/Tests/SearchEndpointTests.cs
+++ Artskart3.Tests.Integration/Tests/SearchEndpointTests.cs
@@ -110,7 +110,7 @@ public class SearchEndpointTests : IAsyncLifetime
         response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
         var json = await response.Content.ReadAsStringAsync();
         json.Should().Contain("error");
-        json.Should().Contain("1").And.Contain("1000"); // Min and Max values
+        json.Should().Contain("1").And.Contain("100000"); // Min and Max values
     }
 
     [Fact]
@@ -122,7 +122,7 @@ public class SearchEndpointTests : IAsyncLifetime
         response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
         var json = await response.Content.ReadAsStringAsync();
         json.Should().Contain("error");
-        json.Should().Contain("between").And.Contain("1").And.Contain("1000");
+        json.Should().Contain("between").And.Contain("1").And.Contain("100000");
     }
 
     [Fact]
