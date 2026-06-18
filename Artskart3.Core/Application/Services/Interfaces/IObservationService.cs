cb68ca3affd6d4900551b18d4ff02098f1edf329
Kristoffer Martinsen <kristoffermartinsen.95@gmail.com>
2026-06-18 11:04:38 +0200

    Endpoints for ObservationDetails and ObservationsByLocations

---
+++ Artskart3.Core/Application/Services/Interfaces/IObservationService.cs
@@ -0,0 +1,10 @@
+﻿
+using Artskart3.Core.Domain.Entities;
+
+namespace Artskart3.Core.Application.Services.Interfaces;
+
+public interface IObservationService
+{
+    Task<Observation> GetObservationDetails(int locationId, int observationId);
+    Task<IEnumerable<Observation>> GetObservationsByLocation(int locationId);
+}
