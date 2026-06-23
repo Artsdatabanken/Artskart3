0fc96a7963ef7402d6838d0a6603d89caaf04ee6
Kristoffer Martinsen <kristoffermartinsen.95@gmail.com>
2026-06-23 16:03:39 +0200

    ObservationDto for observation list by location.

--- Artskart3.Infrastructure/Persistence/Repositories/ObservationRepository.cs
+++ Artskart3.Infrastructure/Persistence/Repositories/ObservationRepository.cs
@@ -1,4 +1,5 @@
-﻿using Artskart3.Core.Application.Persistence;
+﻿using Artskart3.Core.Application.DTOs;
+using Artskart3.Core.Application.Persistence;
 using Artskart3.Core.Domain.Entities;
 using Artskart3.Core.Domain.RepositoryInterfaces;
 using Microsoft.EntityFrameworkCore;
@@ -8,14 +9,20 @@ namespace Artskart3.Infrastructure.Persistence.Repositories;
 
 public class ObservationRepository(IArtsKartDbContext context, ILogger<ObservationRepository> logger) : IObservationRepository
 {
-    public async Task<Observation> GetObservationDetails(int locationId, int observationId)
+    public async Task<ObservationDto> GetObservationDetails(int locationId, int observationId)
     {
         try
         {
             Observation observationDetails = await context.Set<Observation>()
                 .Where(o => (o.LocationId == locationId) && (o.Id == observationId))
                 .FirstAsync();
-            return observationDetails;
+            var observationDto = new ObservationDto
+            {
+                Id = observationDetails.Id,
+                PreferredPopularName = observationDetails.MatchedScientificName.ScientificName,
+                Author = observationDetails.MatchedScientificName.ScientificNameAuthorship,
+            };
+            return observationDto;
         }
         catch (Exception e)
         {
@@ -24,15 +31,23 @@ public class ObservationRepository(IArtsKartDbContext context, ILogger<Observati
         }
     }
 
-    public async Task<IEnumerable<Observation>> GetObservationByLocation(int locationId)
+    public async Task<IEnumerable<ObservationDto>> GetObservationByLocation(int locationId)
     {
         try
         {
             IEnumerable<Observation> observations = await context.Set<Observation>()
+                .Include(o => o.Taxon)
                 .Where(o => o.LocationId == locationId)
                 .ToListAsync();
-
-            return observations;
+            IEnumerable<ObservationDto> observationDto = observations.Select(o => new ObservationDto
+            {
+                Id = o.Id,
+                PreferredPopularName = o.Taxon.PreferredPopularName ?? string.Empty,
+                ScientificName = o.Taxon.ValidScientificName ?? string.Empty,
+                TaxonGroupId = o.TaxonGroupId,
+                Locality = o.LocationId.ToString(),
+            });
+            return observationDto;
         }
         catch (Exception e)
         {
