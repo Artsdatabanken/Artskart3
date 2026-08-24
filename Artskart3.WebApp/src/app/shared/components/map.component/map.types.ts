be920dc16c986fedf84017cf172c2e33c6356014
Kristoffer Martinsen <kristoffermartinsen.95@gmail.com>
2026-08-24 14:53:18 +0200

    Get observation for polygon

--- Artskart3.WebApp/src/app/shared/components/map.component/map.types.ts
+++ Artskart3.WebApp/src/app/shared/components/map.component/map.types.ts
@@ -1,5 +1,24 @@
+import type Feature from 'ol/Feature';
+import type BaseLayer from 'ol/layer/Base';
+
 export enum ApiZoomLevel {
   Counties = 1,
   Municipalities = 2,
   LocationPoints = 3
+}
+
+export interface PointerClickFeature {
+  feature: Feature;
+  layer: BaseLayer;
+  featureId: string;
+  layerId: string;
+  properties?: Record<string, unknown>;
+}
+
+export interface LocationFeatureProperties {
+  id: number;
+  name: string;
+  observationCount: number;
+  observationCountDisplay: string;
+  isPolygon: boolean;
 }
