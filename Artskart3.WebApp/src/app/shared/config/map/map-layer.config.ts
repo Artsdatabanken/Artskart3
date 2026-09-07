8ee95fe1e318b53b1f6d2993c4028d7dc5bb402f
DipShre <dipak.shrestha@artsdatabanken.no>
2026-09-07 11:34:10 +0200

    oppdatering bakgrunnkart close #462

--- Artskart3.WebApp/src/app/shared/config/map/map-layer.config.ts
+++ Artskart3.WebApp/src/app/shared/config/map/map-layer.config.ts
@@ -1,5 +1,6 @@
 export interface MapTypeOption {
   label: string;
+  description?: string;
   layerId: string;
 }
 
@@ -7,6 +8,6 @@ export const MAP_TYPE_OPTIONS: MapTypeOption[] = [
   { label: 'mapToolbar.mapTypes.topografisk', layerId: 'topografisk' },
   { label: 'mapToolbar.mapTypes.topo4graatone', layerId: 'topo4graatone' },
   { label: 'mapToolbar.mapTypes.nib', layerId: 'nib' },
-  { label: 'mapToolbar.mapTypes.svalbard', layerId: 'svalbard' },
-  { label: 'mapToolbar.mapTypes.janmayen', layerId: 'janmayen' },
+  { label: 'mapToolbar.mapTypes.landscape', description: 'mapToolbar.mapTypes.svalbard', layerId: 'svalbard' },
+  { label: 'mapToolbar.mapTypes.landscape', description: 'mapToolbar.mapTypes.janmayen', layerId: 'janmayen' },
 ];
